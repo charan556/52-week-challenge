deb9e9ca43b891443d1538deb9002067b71b8109
charan <charan.cse@gmail.com>
2016-12-22 12:12:35 +0530

    initial commit

---
+++ cucumber-poc/src/main/java/samples/bdd/calculator/Calculator.java
@@ -0,0 +1,14 @@
+package samples.bdd.calculator;
+
+public class Calculator {
+
+	private int sum;
+
+	public void add(int a, int b) {
+		sum = a + b;
+	}
+
+	public int getSum() {
+		return sum;
+	}
+}
