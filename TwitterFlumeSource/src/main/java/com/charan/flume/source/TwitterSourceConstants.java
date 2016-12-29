58e681b94741a9a01e2b6663e4c5d76d2f57553f
charan <charan.cse@gmail.com>
2016-12-29 18:57:19 +0530

    init

---
+++ TwitterFlumeSource/src/main/java/com/charan/flume/source/TwitterSourceConstants.java
@@ -0,0 +1,30 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.charan.flume.source;
+
+public class TwitterSourceConstants {
+
+  public static final String CONSUMER_KEY_KEY = "TqW4fAB69Ec0L4aXBLBsQqeWW";
+  public static final String CONSUMER_SECRET_KEY = "TqW4fAB69Ec0L4aXBLBsQqeWW";
+  public static final String ACCESS_TOKEN_KEY = "324147718-EABym3zdGtGF2zXQrrbijbirF8tWK9Xxp9rM6h5r";
+  public static final String ACCESS_TOKEN_SECRET_KEY = "i8aPX45mGQbLSam9o6kL0d61xsLPaxrfK02XIf8N7xvfv";
+  
+  public static final long DEFAULT_BATCH_SIZE = 1000L;
+  public static final String KEYWORDS_KEY = "dangal";
+}
