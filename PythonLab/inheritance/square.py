f08820dbce85599c9fc12c3189063ebca037aaef
charan <charan.cse@gmail.com>
2017-02-16 18:53:01 +0530

    addded inheritance

---
+++ PythonLab/inheritance/square.py
@@ -0,0 +1,17 @@
+'''
+Created on 16-Feb-2017
+
+@author: charan
+'''
+from inheritance.Shape import Shape
+
+class Square(Shape):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, x):
+        self.x = x
+        self.y = x
+        
