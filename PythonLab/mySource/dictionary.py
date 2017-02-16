9563c31f2fb31dc87a4860fefff1af3014051b76
charan <charan.cse@gmail.com>
2017-02-16 17:03:59 +0530

    added

---
+++ PythonLab/mySource/dictionary.py
@@ -0,0 +1,10 @@
+student = {'Name': 'Charan', 'Age': 31, 'Class': 'First'}
+print ("student['Name']: ", student['Name'])
+print ("student['Age']: ", student['Age'])
+
+#deleteing dictionary Elements 
+del student['Name']
+print(student)
+
+student.clear()
+print(student)
---
+++ PythonLab/mySource/lists.py
@@ -0,0 +1,11 @@
+list1 = ['physics', 'chemistry', 1997, 2000]
+print(list1)
+print(list1[1:2])
+
+#updating list
+list1[2] = 'robotics'
+print(list1)
+
+#deleting element in list
+del list1[3]
+print(list1)
